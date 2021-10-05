6745b0e8dd1036465d2e0b561a2ef00e632d1ae3
IMasGS <ignasi.mas@goldenspear.com>
2021-10-05 08:57:28 +0200

    Specify condition

--- scripts/review_features.py
+++ scripts/review_features.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         feature_md = ''
 
         for feature in features:
-            if db_selector.feat_name_from_id(feature['featId']):
+            if db_selector.feat_name_from_id(feature['featId']) not in ('NONE', 'UNKNOWN'):
                 feature_md += '<div>' + db_selector.feat_name_from_id(feature['featId']) + '</div>'
 
         col1, col2 = st.beta_columns((1, 1))
